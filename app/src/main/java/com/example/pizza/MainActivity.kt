d3519e1573472f8d2ca4ff00dc2e6183b93d1820
Dezignar <torisonra@ya.ru>
2022-08-24 16:52:58 +0900

    темы

--- app/src/main/java/com/example/pizza/MainActivity.kt
+++ app/src/main/java/com/example/pizza/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.pizza
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pizza.ui.theme.AppTheme
 import com.example.pizza.ui.theme.PizzaTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,23 +19,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             PizzaTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Text(text = "Hello Android!",color = AppTheme.colors.primaryTintColor)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PizzaTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/pizza/ui/theme/Color.kt
+++ app/src/main/java/com/example/pizza/ui/theme/Color.kt
@@ -6,11 +6,23 @@ data class Colors (
     val primaryBackground:Color,
     val secondaryBackground: Color,
     val headerTextColor:Color,
-    val primaryTextColor:Color
+    val primaryTextColor:Color,
+    val primaryTextInvertColor:Color,
+    val primaryTintColor:Color,
+    val secondaryTintColor:Color,
+    val hintTextColor:Color,
+    val accentColor: Color,
+    val notificationColor:Color
         )
 val lightPalette = Colors(
     primaryBackground = Color.White,
     secondaryBackground = Color(0xFFD0CCC7),
     primaryTextColor = Color(0xFF59442B),
-    headerTextColor = Color(0xFF311F09)
+    headerTextColor = Color(0xFF311F09),
+    primaryTextInvertColor = Color.White,
+    primaryTintColor = Color(0xFFFF8A00),
+    secondaryTintColor = Color(0xFF3FA72F),
+    hintTextColor = Color(0xFFA0978C),
+    accentColor = Color(0xFF8aEAFF),
+    notificationColor = Color(0xFFFF3838)
 )
